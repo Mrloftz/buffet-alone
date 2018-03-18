963e7075af52654cfd04ae35d6c2a3bf172cfe27
Mrloftz <skro3ert@hotmail.com>
2018-03-18 18:07:32 +0700

    final project

--- app/src/main/java/info/androidhive/navigationdrawer/activity/CreateroomActivity.java
+++ app/src/main/java/info/androidhive/navigationdrawer/activity/CreateroomActivity.java
@@ -1,10 +1,9 @@
 package info.androidhive.navigationdrawer.activity;
 
 import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.icu.util.Calendar;
 import android.os.Build;
 import android.os.Bundle;
@@ -22,7 +21,6 @@ import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -42,10 +40,11 @@ import java.util.Map;
 
 import info.androidhive.navigationdrawer.R;
 import info.androidhive.navigationdrawer.app.AppController;
+import info.androidhive.navigationdrawer.helper.SQLiteHandler;
 
 public class CreateroomActivity extends AppCompatActivity {
     RequestQueue requestQueue;
-    String url ="http://192.168.1.56/loft/createroom.php";
+    String url ="http://192.168.45.2/loft/insertroom.php";
 
 
 
@@ -54,41 +53,65 @@ public class CreateroomActivity extends AppCompatActivity {
     private TextView mDisplayDate;
     private DatePickerDialog.OnDateSetListener mDateSetListener;
     private TextView mTxtTime,placeshow,priceshow;
-    private Spinner spinner;
+    private Spinner spinnerPeopleMax;
     private EditText commentbox;
+    private EditText EditTextTitle;
     private Button Button;
     private ImageView showImg;
+    SQLiteHandler db ;
+    String sqlite_email;
+
+    String pro_id;
+    String email_user;
+    String title,des,peoplemax;
+    private String time,time_end;
+    String date;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_createroom);
         placeshow = (TextView) findViewById(R.id.placeshow);
-        priceshow= (TextView) findViewById(R.id.priceshow);
-        showImg = (ImageView) findViewById(R.id.showImg) ;
+        priceshow = (TextView) findViewById(R.id.priceshow);
+        showImg = (ImageView) findViewById(R.id.showImg);
         mDisplayDate = (TextView) findViewById(R.id.selectdate);
         mTxtTime = (TextView) findViewById(R.id.selecttime);
-        spinner = (Spinner) findViewById(R.id.selectpeople);
         commentbox = (EditText) findViewById(R.id.extraedittext);
         Button = (Button) findViewById(R.id.createbutton);
+        EditTextTitle = (EditText) findViewById(R.id.edtroomname);
+        spinnerPeopleMax=(Spinner) findViewById(R.id.selectpeople);
 
+        db = new SQLiteHandler(this);
+        HashMap<String, String> users = db.getUserDetails();
 
         mDisplayDate.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void onClick(View view) {
+                onCreateDialog(CreateroomActivity.this).show();
+/*
                 Calendar cal = Calendar.getInstance();
                 int year = cal.get(Calendar.YEAR);
                 int month = cal.get(Calendar.MONTH);
                 int day = cal.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog dialog = new DatePickerDialog(this,
+                        android.R.style.Theme_Material_Dialog_NoActionBar_MinWidth,
+                        mDateSetListener,
+                        2013, 2, 18);
+                dialog.create();
+                dialog.show();
+*/
 
-                DatePickerDialog dialog = new DatePickerDialog(getApplicationContext(),
+/*
+                DatePickerDialog dialog = new DatePickerDialog(getBaseContext(),
                         android.R.style.Theme_Material_Dialog_NoActionBar_MinWidth,
                         mDateSetListener,
                         year, month, day);
                 dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 dialog.show();
+*/
             }
         });
+
 
         mDateSetListener = new DatePickerDialog.OnDateSetListener() {
             @Override
@@ -97,36 +120,13 @@ public class CreateroomActivity extends AppCompatActivity {
                 Log.d(TAG, "onDateSet: DD/MM/YYY " + day + " / " + month + " / " + year);
 
                 String date = day + " / " + month + " / " + year;
-                mDisplayDate.setText(date);
+
             }
         };//>ส่วนของการเลือกวันที่
         Button.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View view) {
-
-                StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-
-                    }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-
-                    }
-                }) {
-                    @Override
-                    protected Map<String, String> getParams() throws AuthFailureError {
-                        Map<String, String> parameter = new HashMap<String, String>();
-                        parameter.put("peoplemax",spinner.getContext().toString());
-                        parameter.put("d_create",mDisplayDate.getText().toString());
-                        parameter.put("t_create",mTxtTime.getText().toString());
-                        parameter.put("des",commentbox.getText().toString());
-                        return parameter;
-
-                    }
-                };
-                requestQueue.add(request);
+                create_room();
             }
         });
         //<ส่วนของการเลือกเวลา
@@ -134,6 +134,10 @@ public class CreateroomActivity extends AppCompatActivity {
             @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void onClick(View v) {
+
+                onCreateTimeDialog(CreateroomActivity.this).show();
+
+                /*
                 Calendar cal2 = Calendar.getInstance();
                 int hour = cal2.get(Calendar.HOUR_OF_DAY);
                 int minute = cal2.get(Calendar.MINUTE);
@@ -147,7 +151,7 @@ public class CreateroomActivity extends AppCompatActivity {
                     }
                 }, hour, minute, true);
                 timePickerDialog.show();
-
+*/
             }
         });//>ส่วนของการเลือกเวลา
 
@@ -161,18 +165,69 @@ public class CreateroomActivity extends AppCompatActivity {
                 getApplicationContext(), R.layout.spinner_item, numberList);
 
         spinnerArrayAdapter.setDropDownViewResource(R.layout.spinner_item);
-        spinner.setAdapter(spinnerArrayAdapter);
+        spinnerPeopleMax.setAdapter(spinnerArrayAdapter);
         //>ส่วนของเลือกจำนวนคน
         // Inflate the layout for this fragment
 
-
-
         Intent intent = getIntent();
-        String id = intent.getStringExtra("pro_id");
-        getpro(id,"http://192.168.45.2/loft/getpro.php");
+        //รวมข้อมูลไปส่ง
+        pro_id = intent.getStringExtra("pro_id");
+        sqlite_email = users.get("email").toString();
+        //pro = Integer.parseInt(pro_id);
+        //
+        getpro(pro_id,"http://192.168.45.2/loft/getpro.php");
     }
 
+    public Dialog onCreateDialog(CreateroomActivity savedInstanceState) {
+
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+
+        return new DatePickerDialog(this, dateSetListener, year, month, day);
+    }
+
+    private DatePickerDialog.OnDateSetListener dateSetListener =
+            new DatePickerDialog.OnDateSetListener() {
+                public void onDateSet(DatePicker view, int year, int month, int day) {
+                    Toast.makeText(CreateroomActivity.this, "selected date is " + view.getYear() +
+                            " / " + (view.getMonth()+1) +
+                            " / " + view.getDayOfMonth(), Toast.LENGTH_SHORT).show();
+                    //String date1 = view.getDayOfMonth() + "/"  + (view.getMonth()+1) + "/" + view.getYear();
+                    //Log.d("test" , date1);
+                    date = ""+view.getYear()+"-"+(view.getMonth()+1)+"-"+view.getDayOfMonth();
+                    mDisplayDate.setText(date);
+                }
+            };
+
+    public Dialog onCreateTimeDialog(CreateroomActivity savedInstanceState) {
+
+        Calendar cal2 = Calendar.getInstance();
+        int hour = cal2.get(Calendar.HOUR_OF_DAY);
+        int minute = cal2.get(Calendar.MINUTE);
+
+        return new TimePickerDialog(this, au , hour, minute, true);
+    }
+
+    private TimePickerDialog.OnTimeSetListener au =
+            new TimePickerDialog.OnTimeSetListener() {
+                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+                    Log.i("Time", "hours: " + hourOfDay + " minute: " + minute);
+                    //time = hourOfDay + ":" + minute + " น.";
+                    time = ""+hourOfDay +":"+ minute ;
+                    time_end = ""+(hourOfDay+4) + ":" + minute;
+
+
+
+                    mTxtTime.setText(time);
+                }
+            };
+
     private void getpro(final String pro_id , String URL) {
+
         // Tag used to cancel the request
         String tag_string_pro = "buffet tag";
 
@@ -227,6 +282,49 @@ public class CreateroomActivity extends AppCompatActivity {
     }
 
 
+    private void create_room() {
+        title = EditTextTitle.getText().toString();
+        des = commentbox.getText().toString();
+        peoplemax = spinnerPeopleMax.getSelectedItem().toString();
 
-        }
+        // Tag used to cancel the request
+        String tag_string_pro = "buffet tag";
+
+        StringRequest strReq = new StringRequest(Request.Method.POST,
+                url, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, "Profile Response for getpro: " + response.toString());
+
+            }
+        }, new com.android.volley.Response.ErrorListener() {
+
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Profile Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() {
+                // Posting params to register url
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("pro_id", pro_id);
+                params.put("user_header", sqlite_email);
+                params.put("title", title);
+                params.put("des", des);
+                params.put("people_max", peoplemax);
+                params.put("t_start", date+"T"+time);
+                return params;
+            }
+
+        };
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(strReq, tag_string_pro);
+    }
+
+}
 
